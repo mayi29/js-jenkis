e02c5b306a89e3e81f16f23b5782e0114f1d075a
Mayerly Caro León <mayerlycaro@MacBook-Air-de-Mayerly.local>
2020-03-25 17:57:00 -0500

    primer commit

---
+++ holamundo.js
@@ -0,0 +1,22 @@
+function startTime()
+		{
+			var today=new Date();
+			var h=today.getHours();
+			var m=today.getMinutes();
+			var s=today.getSeconds();
+
+	m=checkTime(m);
+	s=checkTime(s);
+		
+		document.getElementById('date').innerHTML=h+":"+m;
+		t=setTimeout('startTime()',500);
+		}
+
+		function checkTime(i)
+		{
+			if (i<10)
+			{
+				i="0" + i;
+				}
+			return i;
+			}
